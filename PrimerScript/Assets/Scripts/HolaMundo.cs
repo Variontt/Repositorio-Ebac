ac1874661edfcec74804e07eba649f56e78d2fa7
Vladimir <rojo.alejandro73@gmail.com>
2022-09-05 18:43:57 -0500

    Modulo 05 Tarea
    Tarea donde usamos Log, Log,Error y Log,Warning

--- PrimerScript/Assets/Scripts/HolaMundo.cs
+++ PrimerScript/Assets/Scripts/HolaMundo.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    private void Awake()
+    {
+        
+    }
+    int x; 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hola Mundo");
+        x = 0;
+        print("algo paso");
+
+        Debug.LogWarning("Algo salio medianamente Mal");
+        Debug.LogError("Algo salio muy mal");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log("Hola desde update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde late Update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto a sido habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("El objeto a sido inhabilitado");
     }
 }
